3b94829a1a21c42a1d5c58549d56ca1a3dc1b844
Vahe <67160168+vahe-arzumanyan@users.noreply.github.com>
2022-11-16 00:27:51 +0400

    change

--- modal-app/src/App.jsx
+++ modal-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.scss';
 import './index';
+import './assets/style/index.scss'
+import main from './assets/image/main.jpg';
 
 class App extends React.Component {
     render() {
-        return
-        <div></div>
+        return <header className={'G-image-cover P-header-image-cover'} style={{backgroundImage: `url(${this.props.main})`}}>
 
+        </header>
 
     }
 }
