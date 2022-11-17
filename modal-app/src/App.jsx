ddceefe779d3b2442a93407ab315b466c7c23035
vahe-arzumanyan <vahearzumanyan01@gmail.com>
2022-11-17 14:14:39 +0400

    new

--- modal-app/src/App.jsx
+++ modal-app/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import './App.scss';
 import './index';
-import './assets/style/index.scss'
-import main from './assets/image/main.jpg';
+import './assets/style/index.scss';
+import Home from './components/Home/index';
+
 
 class App extends React.Component {
     render() {
-        return <header className={'G-image-cover P-header-image-cover'} style={{backgroundImage: `url(${this.props.main})`}}>
-
-        </header>
-
+        return(
+            <Home />
+            )
     }
 }
 
---
+++ modal-app/src/components/Home/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './style.scss';
+import wrapper from '../../assets/image/simple-house-01.jpg';
+import logo from '../../assets/image/simple-house-logo.png';
+
+class Home extends React.Component {
+    render() {
+        return<>
+            <div className={'G-image-cover P-wrapper'} style={{backgroundImage: `url('${wrapper}')`}}>
+                <div className={'G-flex G-flex-row'}>
+                    <div className={'G-image-cover P-wrapper-logo'} style={{backgroundImage: `url('${logo}')`}}></div>
+                    <div className={'G-flex G-flex-column P-wrapper-logo-info'}>
+                        <p className={'P-wrapper-logo-title'}>Simple House</p>
+                        <p className={'P-wrapper-logo-text'}>new restaurant template</p>
+                    </div>
+                </div>
+            </div>
+
+            </>
+    }
+}
+
+export default Home;
