25ab7f3e617164d2636f7815b1d5e0d5bfdc32a5
Vahe <67160168+vahe-arzumanyan@users.noreply.github.com>
2022-11-24 18:27:10 +0400

    asd

--- modal-app/src/App.jsx
+++ modal-app/src/App.jsx
@@ -14,7 +14,10 @@ class App extends React.Component {
         borderR: '',
         titleFontSize: 16,
         descriptionFontSize: 18,
-        uploadFile: ''
+        uploadFile: '',
+        titleBgColor: '',
+        descriptionBgColor: '',
+        borderRadius: 0
     }
     // this is title content
     handelBodyBg = (e) => {
@@ -30,17 +33,28 @@ class App extends React.Component {
     handleTitleColor = (e) => {
         this.setState({titleColor: e.target.value})
     }
+
+    handleTitleBgeColor = (e) => {
+        this.setState({titleBgColor: e.target.value})
+    }
     //this is description content
 
     handleChangeDescription = (e) => {
         this.setState({description: e.target.value})
     }
-
     handleChangeDescriptionFontSize = (e) => {
         this.setState({descriptionFontSize: e.target.value})
     }
     handleChangeDescriptionColor = (e) => {
         this.setState({descriptionColor: e.target.value})
+    }
+    handleDescriptionBgColor = (e) => {
+        this.setState({descriptionBgColor: e.target.value})
+    }
+
+    // border radius
+    handleBorderRadius = (e) => {
+        this.setState({borderRadius: e.target.value})
     }
 
     render() {
@@ -74,6 +88,10 @@ class App extends React.Component {
                                 <input type='color' value={this.state.titleColor}
                                        onChange={this.handleTitleColor}/>
                             </label>
+                            <label> Background Color
+                                <input type='color' value={this.state.titleBgColor}
+                                       onChange={this.handleTitleBgeColor}/>
+                            </label>
                         </div>
 
                         {/* ----------- Description ----------- */}
@@ -92,32 +110,44 @@ class App extends React.Component {
                                 <input type='color' value={this.state.descriptionColor}
                                        onChange={this.handleChangeDescriptionColor}/>
                             </label>
+                            <label> Background Color
+                                <input type='color' value={this.state.titleBgColor}
+                                       onChange={this.handleDescriptionBgColor}/>
+                            </label>
 
                         </div>
                     </div>
-
 
                     {/* ============================= result content ============================= */}
 
-
-                    <div className={'P-result'}>
+                    <div className={'P-result'}  style={{borderRadius:this.state.borderRadius + "px"}}>
                         <div className={'G-flex G-flex-column'}>
                             <div className={'P-result-title'}>
                                 <h3>Title</h3>
-                                <p className={'P-result-title'} style={{
+                                <p className={'hh'} style={{
                                     fontSize: this.state.titleFontSize + 'px',
-                                    color: this.state.titleColor
+                                    color: this.state.titleColor,
+                                    backgroundColor: this.state.titleBgColor
                                 }}>{this.state.title}</p>
                             </div>
                             <div className={'P-result-description'}>
-                                <h3>Descriton</h3>
-                                <p className={'P-result-description'} style={{
+                                <h3>Description</h3>
+                                <p className={'hh'} style={{
                                     fontSize: this.state.descriptionFontSize + 'px',
-                                    color: this.state.descriptionColor
+                                    color: this.state.descriptionColor,
+                                    backgroundColor: this.state.descriptionBgColor
                                 }}>{this.state.description}</p>
                             </div>
                         </div>
+
                     </div>
+
+                </div>
+                <div>
+                    <label>Event Radius
+                        <input type='range' value={this.state.borderRadius} max={50} min={5}
+                               onChange={this.handleBorderRadius}/>
+                    </label>
                 </div>
             </div>
         </div>
