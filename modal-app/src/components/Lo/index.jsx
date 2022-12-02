f43780c0414c2e837023eee0c9be207a4cd27edf
Vahe <67160168+vahe-arzumanyan@users.noreply.github.com>
2022-12-03 03:05:03 +0400

    sa

---
+++ modal-app/src/components/Lo/index.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+
+
+const Lo = ({data})=>{
+
+    return <div>
+<p>{data.name}</p>
+<p>{data.info}</p>
+    </div>
+}
+
+export default Lo;
--- modal-app/src/components/roundBox/index.jsx
+++ modal-app/src/components/roundBox/index.jsx
@@ -1,44 +1,44 @@
 import React, {useEffect, useState} from "react";
 import './style.scss';
+import Lo from "../Lo";
 
 const RoundBox = () => {
     const [changeBgColor, setChangeBgColor] = useState([]);
-    const [windLoader, setWinLoader]=useState(true)
+    const [windLoader, setWinLoader] = useState(true)
 
     const handelChangeBgColor = () => {
-        if (setWinLoader.length) {
-            setWinLoader(false)
+        setWinLoader(false)
         }
-
-    }
 
     useEffect(() => {
 
-        setWinLoader(windLoader)
+     if (!windLoader) {
 
-        setChangeBgColor([
-            {
-                name: 'uf you want change background color',
-                info: 'then ...'
-            }
-        ])
+         setChangeBgColor([
+                 {
+                     name: 'uf you want change background color',
+                     info: 'then ...'
+                 }
+             ]
+         )
 
-    })
-
-
-
-
+     }})
 
 
     return <div className={'G-container G-flex G-center'}>
         <div className={'P-random-box G-flex G-center G-flex-column'}>
+                {handelChangeBgColor.length ? setChangeBgColor.map((index, item) => {
+                    return <Lo key={index}
+                               data={item}
+                    />
+                }) : null}
 
-            <button onClick={handelChangeBgColor}>click me</button>
-        </div>
+
+                <button onClick={handelChangeBgColor}>click me</button>
+            </div>
     </div>
 
+        }
 
-}
-
-export default RoundBox;
+        export default RoundBox;
 
