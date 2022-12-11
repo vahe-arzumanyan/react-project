fd700c69cae026fb84ddb7b86527c5e4c094364c
Vahe <67160168+vahe-arzumanyan@users.noreply.github.com>
2022-12-11 19:55:35 +0400

    aa

--- modal-app/src/App.jsx
+++ modal-app/src/App.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import './App.scss';
 
 
-import {Switch, Route, NavLink, Link, Redirect} from 'react-router-dom';
+// import {Switch, Route, NavLink, Link, Redirect} from 'react-router-dom';
 
 
 const App = () => {
     return <div>
-<p>Hello stf branch</p>
-        {/*react js default*/}
-        {/*function component*/}
-        {/*this is stf branch*/}
-        {/*css style:none*/}
+
+
     </div>
 }
 
