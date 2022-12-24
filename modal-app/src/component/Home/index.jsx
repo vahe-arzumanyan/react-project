519c21bd38646e949c396cb677ee80895524c7c4
vahe-arzumanyan <vahearzumanyan01@gmail.com>
2022-12-24 12:32:34 +0400

    ss

--- modal-app/src/component/Home/index.jsx
+++ modal-app/src/component/Home/index.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import './style.scss'
-import {NavLink, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import School from "../../pages/school";
 import Teacher from "../../pages/teachers";
 import Pupil from "../../pages/pupils";
 
 const Home = () => {
-    return <div className={'P-top-ui G-flex G-center'}>
-        <p>Schools administration</p>
+    return <div className='P-top-ui G-flex G-center'>
+        <div className='P-school-admin-title'>
+            <p>Schools administration</p>
+        </div>
+            <Routes>
+                <Route path={"/School"} element={<School/>} />
+                <Route path={"/Teachers"} element={<Teacher/>} />
+                <Route path={"/Pupil"} element={<Pupil/>} />
+                <Route path= {'/Home'} element={<School/>}/>
+            </Routes>
 
-
-        <Routes>
-
-            <Route path={"/School"} element={<School/>} />
-            <Route path={"/Teachers"} element={<Teacher/>} />
-            <Route path={"/Pupil"} element={<Pupil/>} />
-            <Route path= "*" element={<School/>}/>
-        </Routes>
     </div>
+
 }
 
 export default Home;
--- modal-app/src/component/schoolCategory/index.jsx
+++ modal-app/src/component/schoolCategory/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import './style.scss';
-import {NavLink, Route, Routes} from 'react-router-dom';
-import School from "../../pages/school";
-import Teacher from "../../pages/teachers";
-import Pupil from "../../pages/pupils";
+import {NavLink} from 'react-router-dom';
+
 
 const SchoolCategory = () => {
     return <div className={'P-school-action-title G-flex G-flex-column G-align-center'}>
-        <div className={'P-admin-action-title G-flex G-center'}>
+        <div className='P-admin-action-title G-flex G-center'>
             <p>Administration Action</p>
         </div>
-        <ul>
+
+        <ul className='P-category-ul'>
             <li>
                 <NavLink to={'/School'}><a>Schools</a></NavLink>
             </li>
@@ -18,7 +17,7 @@ const SchoolCategory = () => {
                 <NavLink to={'/Teacher'}><a>Teachers</a></NavLink>
             </li>
             <li>
-                <NavLink to={'Pupil'}><a>Pupils</a></NavLink>
+                <NavLink to={'/Pupil'}><a>Pupils</a></NavLink>
             </li>
         </ul>
 
